7d76718bec40a30f9b6f71a82185c35a7bf7cbc6
Gustavo Cardoso <contato.gusttavo08@gmail.com>
2022-03-23 19:16:51 -0300

    aula 01

---
+++ aula.js
@@ -0,0 +1,15 @@
+// let idade = 25;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+// não posso iniciar variáveis com números, espaços
+// utilizar sempre o camelCase
+// lembrando que JS é uma linguagem case sensitive
+
+// quando a variavel conter letras, sempre lembrar das aspas
+let nome = 'Gustavo';
+console.log(nome)
+let idade = 25;
+console.log(idade);
+let altura = 170;
+console.log(altura);
