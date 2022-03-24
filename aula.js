382ab29f8c1e2887aea59ac4f077bb68ba197abf
Gustavo Cardoso <contato.gusttavo08@gmail.com>
2022-03-24 01:45:47 -0300

    tipos primitivos

--- aula.js
+++ aula.js
@@ -1,17 +1,7 @@
-// let idade = 25;
-// console.log(idade);
-// let altura = 180;
-// console.log(altura);
-// não posso iniciar variáveis com números, espaços
-// utilizar sempre o camelCase
-// lembrando que JS é uma linguagem case sensitive
+// Tipos Primitivos
 
-// quando a variavel conter letras, sempre lembrar das aspas
-
-let valorIngressoAdulto = 20;
-// posso simplesmente atualizar o valor da variável citando-a novamente, sem precisar mudar o valor da primeira vez em que a inseri:
-valorIngressoAdulto = 30;
-// o novo valor é impresso no console, ao invés do primeiro
-console.log(valorIngressoAdulto)
-// lembrando que só podemos fazer isso com a variável 'let'. Caso mudemos para a variável 'const', não podemos redefinir, já que o valor obrigatoriamente deve ser constante.
-// é uma boa prática utilizarmos apenas o 'const'. Usar o 'let' apenas caso seja necessário atualizar sempre o valor da variável.
+let nome = 'Gustavo'; //string literal, armazena informações como em formato String
+let idade = 25; // chama-se number literal
+let estaAprovado = true; // boolean, ajuda a construir regras
+let sobrenome = undefined ; // undefined, não possui definição, pouco usado
+let corSelecionado = null; // quando quisermos redefinir um valor sempre
