7cd1eb59719dee4f4cad5270f261278e11504ba0
Gustavo Cardoso <contato.gusttavo08@gmail.com>
2022-03-24 01:37:21 -0300

    aula 02

--- aula.js
+++ aula.js
@@ -7,9 +7,11 @@
 // lembrando que JS é uma linguagem case sensitive
 
 // quando a variavel conter letras, sempre lembrar das aspas
-let nome = 'Gustavo';
-console.log(nome)
-let idade = 25;
-console.log(idade);
-let altura = 170;
-console.log(altura);
+
+let valorIngressoAdulto = 20;
+// posso simplesmente atualizar o valor da variável citando-a novamente, sem precisar mudar o valor da primeira vez em que a inseri:
+valorIngressoAdulto = 30;
+// o novo valor é impresso no console, ao invés do primeiro
+console.log(valorIngressoAdulto)
+// lembrando que só podemos fazer isso com a variável 'let'. Caso mudemos para a variável 'const', não podemos redefinir, já que o valor obrigatoriamente deve ser constante.
+// é uma boa prática utilizarmos apenas o 'const'. Usar o 'let' apenas caso seja necessário atualizar sempre o valor da variável.
