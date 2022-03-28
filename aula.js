dde2893d2f89a009a38cfcc684eb53111f9e7955
Gustavo Cardoso <contato.gusttavo08@gmail.com>
2022-03-28 14:16:26 -0300

    boas práticas

--- aula.js
+++ aula.js
@@ -5,3 +5,6 @@ let idade = 25; // chama-se number literal
 let estaAprovado = true; // boolean, ajuda a construir regras
 let sobrenome = undefined ; // undefined, não possui definição, pouco usado
 let corSelecionado = null; // quando quisermos redefinir um valor sempre
+
+console.log(nome) //podemos concatenar informações, ficando assim: console.log(nome,idade), mas isso não é uma boa prática
+console.log(idade) // uma boa prática é invocar uma única informação por "console.log"
